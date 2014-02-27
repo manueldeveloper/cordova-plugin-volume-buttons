04bd2c323f3530dfa9db033d7a3caa4e023d161a
Manuel Flores Arribas <manueldeveloper@gmail.com>
2014-02-27 20:30:03 +0100

    Resolved one performance bug, added the documentation in the javascript file and added the license headers

--- src/android/VolumeButtonsListener.java
+++ src/android/VolumeButtonsListener.java
@@ -1,3 +1,22 @@
+/*
+	   Licensed to the Apache Software Foundation (ASF) under one
+	   or more contributor license agreements.  See the NOTICE file
+	   distributed with this work for additional information
+	   regarding copyright ownership.  The ASF licenses this file
+	   to you under the Apache License, Version 2.0 (the
+	   "License"); you may not use this file except in compliance
+	   with the License.  You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+	   Unless required by applicable law or agreed to in writing,
+	   software distributed under the License is distributed on an
+	   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+	   KIND, either express or implied.  See the License for the
+	   specific language governing permissions and limitations
+	   under the License.
+*/
+
 package com.manueldeveloper;
 
 import org.apache.cordova.CallbackContext;
@@ -47,12 +66,8 @@ public class VolumeButtonsListener extends CordovaPlugin implements OnKeyListene
 	*/
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-		Log.d(VolumeButtonsListener_LOG, "Execute method called");
-
 		// Check the action
 		if( action.equals("start") ){
-
-			Log.d(VolumeButtonsListener_LOG, "Start action");
 
 			// Check if the plugin is listening the volume button events
 			if( this.volumeCallbackContext != null ){
@@ -73,9 +88,8 @@ public class VolumeButtonsListener extends CordovaPlugin implements OnKeyListene
 		}
 		else if( action.equals("stop") ){
 
-			Log.d(VolumeButtonsListener_LOG, "Stop action");
-
 			// Erase the callbacks reference and stop the listening process
+			sendSignal(new JSONObject(), false); // release status callback in Javascript side
 			this.volumeCallbackContext= null;
 			this.webView.setOnKeyListener(null);
 			callbackContext.success();
--- www/volumebuttons.js
+++ www/volumebuttons.js
@@ -1,3 +1,27 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+*/
+
+/**
+* 	This class lets to handle the events which have emitted by the volume buttons of the device
+*/
 var cordova= 	require('cordova'),
 	exec= 		require('cordova/exec');
 	
@@ -12,6 +36,9 @@ var VolumeButtons= function(){
 		this.channels[key].onHasSubscribersChange= VolumeButtons.onHasSubscribersChange;
 };
 
+/**
+* 	Keep track of how many handlers we have so we can start and stop the native listener appropriately
+*/
 VolumeButtons.onHasSubscribersChange= function(){
 	
 	if(this.numHandlers === 1 && handlers() === 1)
@@ -20,12 +47,20 @@ VolumeButtons.onHasSubscribersChange= function(){
 		exec(null, null, "VolumeButtons", "stop", []);
 };
 
+/**
+* 	Callback used when the user presses the volume button of the device
+*
+*	@param {Object} info	keys: signal ['volume-up' or 'volume-down'] 
+*/
 VolumeButtons.prototype.volumeButtonsListener= function(info){
 	if(info){
 		cordova.fireWindowEvent("volumebuttonslistener", info);
 	}
 };
 
+/**
+* 	Error callback for listener start
+*/
 VolumeButtons.prototype.errorListener= function(e){
 	console.log("Error initializing VolumeButtons: " + e);
 };
